b35a23245f8442c175ec86221f2e9320922d2e92
pibouill <pibouill@student.42prague.com>
2024-03-27 21:11:39 +0100

    fdsfd

--- inc/minitalk.h
+++ inc/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: pibouill <pibouill@student.42prague.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 17:03:20 by pibouill          #+#    #+#             */
-/*   Updated: 2024/03/27 17:04:18 by pibouill         ###   ########.fr       */
+/*   Updated: 2024/03/27 17:08:06 by pibouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include "libft.h"
 
 
 
