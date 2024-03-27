eb23170912711a9f0d2bcc4a869b308c17cc88dd
pibouill <pibouill@student.42prague.com>
2024-03-27 17:04:57 +0100

    test

---
+++ inc/minitalk.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pibouill <pibouill@student.42prague.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/27 17:03:20 by pibouill          #+#    #+#             */
+/*   Updated: 2024/03/27 17:04:18 by pibouill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+
+
+
+
+#endif
